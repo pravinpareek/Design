5391ce5415c4d439c2d75d92f1e5a32a307763b9
Pravin Pareek <pareek.pravin@gmail.com>
2017-07-10 09:30:49 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#send-mail').click(function(){
+        $('.alert').show() // To Show alert
+    });
+    $('#label-attach-file').click(function(){
+    	$('#attach-file').click(); //To triger file upload
+    });
+});
